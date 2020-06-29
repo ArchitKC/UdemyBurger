52e3941efda48ffe8a8b027fe7e948063e1a54cb
ArchitKC <archithrd@gmail.com>
2020-06-29 02:06:52 -0400

    changes in thunk

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
-import {createStore} from 'redux';
+import {BrowserRouter} from 'react-router-dom';
+import {createStore,applyMiddleware,compose} from 'redux';
 import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import burgerBuilder from './store/reducers/burgerBuilder'
 
 
-const store=createStore(burgerBuilder);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store=createStore(burgerBuilder,composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 
 const app=(
